8d40a7493afa71ce4c167517257966d5d6bba225
wth is a pull request <117620492+PalaboiCarti@users.noreply.github.com>
2024-02-19 12:18:58 +0800

    even more efficient
    lmao i rly dont know why i have to let myself suffer sometimes

--- incrementShit.cs
+++ incrementShit.cs
@@ -1,30 +1,32 @@
 using System;
-
-public class iHateYou {
-    static void incrementShit(int num){
-        Console.Write("Formula: ");
-        int sum = 0;
-        for(int i=1; i<=num; i++){
-            Console.Write(i);
-            if(i < num){
-                Console.Write("+");
-            }
-            sum = sum + i;
-        }
-        Console.WriteLine(" ");
-        Console.WriteLine(sum);
-    }    
-    static void thingDoer(){
+public class iHateYou
+{
+    static void thingDoer()
+    {
         Console.WriteLine("Give me a goddamn number: ");
         int num = Convert.ToInt32(Console.ReadLine());
-        if(num <= 0){
+        if(num <= 0)
+        {
             Console.WriteLine("Try again fucker");
             thingDoer();
-        }else{
-            incrementShit(num);
+        }else
+        {
+            Console.Write("Formula: ");
+            int sum = 0;
+            for(int i=1; i<=num; i++)
+            {
+                Console.Write(i);
+                if(i < num){
+                    Console.Write("+");
+                }
+                sum = sum + i;
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine(sum);
         }
     }
-    public static void Main(string[] args){
+    public static void Main(string[] args)
+    {
         thingDoer();
     }  
 }
